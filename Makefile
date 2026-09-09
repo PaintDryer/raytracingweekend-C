39bcaa099a1120e149f9e5b35f0c0fa8f43d7672
PaintDryer <adamjohari4@gmail.com>
2026-09-09 18:33:46 +0800

    vec3.h + Makefile + folders

---
+++ Makefile
@@ -0,0 +1,10 @@
+CFLAGS = -Wall -Werror -Wextra -fsanitize=address -lm -I include/
+raytracer: src/main.o
+	$(CC) $(CFLAGS) $^ -o raytracer
+
+src/main.o:
+	$(CC) src/main.c -c $(CFLAGS) $^ -o src/main.o
+
+clean:
+	rm -rf raytracer
+	rm -rf src/*.o
---
+++ include/vec3.h
@@ -0,0 +1,89 @@
+#ifndef VEC3_H
+#define VEC3_H
+
+#include <math.h>
+
+typedef struct
+{
+	double x;
+	double y;
+	double z;
+} vec3;
+
+typedef vec3 point3;
+
+static inline vec3 newv3(double x, double y, double z)
+{
+	return (vec3){ .x = x,
+				   .y = y,
+				   .z = z };
+}
+
+// hardcoded vectors
+static inline vec3 v3zero()
+{
+	return newv3(0.0, 0.0, 0.0);
+}
+
+static inline vec3 v3one()
+{
+	return newv3(1.0, 1.0, 1.0);
+}
+
+// basic operations
+static inline vec3 v3add(vec3 *v, const vec3 *a)
+{
+	return newv3( v->x + a->x,
+					v->y + a->y,
+					v->z + a->z );
+}
+
+static inline vec3 v3sub(vec3 *v, const vec3 *a)
+{
+	return newv3( v->x - a->x,
+				  v->y - a->y,
+				  v->z - a->z );
+}
+	
+static inline vec3 v3mul(vec3 *v, const double i)
+{
+	return newv3( v->x * i,
+				  v->y * i,
+				  v->z * i );
+}
+
+static inline vec3 v3div(vec3 *v, const double i)
+{
+	return newv3( v->x / i,
+				  v->y / i,
+				  v->z / i );
+}
+
+// vector operations
+static inline double v3dot(const vec3 *u, const vec3 *v)
+{
+	return ( u->x * v->x
+		   + u->y * v->y
+		   + u->z * v->z);
+} 
+
+static inline double v3len_sq(const vec3 *v)
+	{ return (v3dot(v, v)); }
+
+static inline double v3len(const vec3 *v)
+	{ return sqrt(v3len_sq(v)); }
+
+static inline vec3 v3unit(const vec3 *v)
+{
+	double len = v3len(v);
+	return newv3( v->x / len , v->y / len, v->z / len );
+}
+
+static inline vec3 v3cross(const vec3 *u, const vec3 *v)
+{
+	return newv3( u->y * v->z - u->z * v->y,
+				  u->z * v->x - u->x * v->z,
+				  u->x * v->y - u->y * v->x );
+}
+
+#endif
